40c1199e647ee0de89d9ec39ed45326cd66b8d00
anupadkh <tutor@anup.pro.np>
2023-01-16 14:43:26 +0545

    git actions add

---
+++ add_to_home_dir.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python3
+
+import os
+
+import sys
+from pathlib import Path
+import re
+y = re.compile("href\s*=\s*\"..")
+ysrc = re.compile("src\s*=\s*\"..")
+
+def main():
+    os.chdir('html')
+    files = os.listdir('.')
+    for x in files:
+        file_read = open(x, 'r+')
+        file_wt = open(os.path.join('..', x), 'w+')
+        line = file_read.read()
+        fx = y.sub('href=\"', line)
+        fx = ysrc.sub('src=\"', line)
+        file_wt.write(fx)
+        file_wt.close()
+        file_read.close()
+
+
+if __name__ == "__main__":
+    main()
